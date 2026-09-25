b604d447395980fe3aee52725865c7c24acb4122
dandy182 <danielcely1@gmail.com>
2026-09-25 17:53:40 -0500

    funcion compliladora de sass lista

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
-import {src, dest, watch} from 'sass';
+import {src, dest, watch} from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import sourceMaps from 'gulp-sourcemaps';
 
 const sass = gulpSass(dartSass);
 
@@ -8,12 +9,22 @@ const sass = gulpSass(dartSass);
 /* rutas */
 const routes = {
     styles:'./src/scss/**/*.scss',
-    dest:'./build'
+    destination:'./build'
 }
 
 
-export function Css(done){
-    src(routes.styles)
-        .pipe(sass({outpustyle:'compress'}).onError(sass.log))
-        .pipe()
+export function compilarCss(done){
+    src('./src/scss/**/*.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass({style:'compressed'}).on('Error', sass.logError))
+        .pipe(sourceMaps.write())
+        .pipe(dest(`./build/css`))
+        done()
+}
+
+
+
+
+export function dev(){
+    watch(`${routes.styles}`, compilarCss)
 }
--- index.php
+++ index.php
@@ -4,8 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial scale=1.0">
         <title>Bienes Raices</title>
+        <link rel="stylesheet" href="./build/css/app.css">
     </header>
     <body>
+        <h1>Hola mundo</h1>
 
     </body>
 </html>
